4308d5650a9122db2f9f104d412bf874c4968d8c
boomerhk <motion4hk@gmail.com>
2016-12-31 11:13:43 +0800

    Final Year Project

---
+++ app/models/customer.rb
@@ -0,0 +1,17 @@
+class Customer < ApplicationRecord
+  validates_presence_of :customer_code
+  validates_presence_of :customer_name
+  validates_presence_of :email
+  validates_presence_of :phone_no
+  validates_presence_of :address
+  validates_presence_of :join_date
+  validates_length_of :customer_code, :is =>5
+  validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  validates_uniqueness_of :customer_code
+  validates_uniqueness_of :customer_name
+
+  self.primary_key = :customer_code
+  has_many :customer_staffs, :class_name => CustomerStaff, :foreign_key => :customer_code
+  has_many :tickets, :class_name => Ticket, :foreign_key => :customer_code
+  has_many :rack_infos
+end
---
+++ app/models/rack_info.rb
@@ -0,0 +1,16 @@
+class RackInfo < ApplicationRecord
+  validates_presence_of :rack_no
+  validates_presence_of :tpye
+  validates_presence_of :customer_code
+  validates_presence_of :total_unit
+  validates_presence_of :low_unit
+  validates_presence_of :height_unit
+  validates_length_of :customer_code, :is =>5
+  validates_numericality_of :total_unit, :only_integer => true
+  validates_numericality_of :low_unit, :only_integer => true
+  validates_numericality_of :height_unit, :only_integer => true
+
+  belongs_to :customer, :class_name => Customer, :foreign_key => :customer_code
+  has_many :temperatures, :foreign_key => :rack_no
+  belongs_to :customer
+end
